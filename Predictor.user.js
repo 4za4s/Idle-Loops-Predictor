5fd11cd42ba767c513b0319b5277ab41aeac2ccf
4za4s <arbo3802@gmail.com>
2020-02-18 20:51:41 +1100

    auto upload code

--- Predictor.user.js
+++ Predictor.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         IdleLoops Predictor 4_za_4s
 // @namespace    https://github.com/4za4s/
-// @version      0.2.3
+// @version      0.3
 // @description  Predicts the amount of resources spent and gained by each action in the action list.
 // @author       4_za_4s
 // @match        *://omsi6.github.io/loops/*
